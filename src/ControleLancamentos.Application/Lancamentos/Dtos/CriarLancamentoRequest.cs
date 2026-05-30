c6417c39c342fd0c823060b82d4985e1da363316
BENNER\leonardo.cadete <leonardo.cadete@benner.com.br>
2026-05-30 12:54:21 -0300

    ajuste no request de criar lancamentos

--- src/ControleLancamentos.Application/Lancamentos/Dtos/CriarLancamentoRequest.cs
+++ src/ControleLancamentos.Application/Lancamentos/Dtos/CriarLancamentoRequest.cs
@@ -6,12 +6,17 @@ namespace ControleLancamentos.Application.Lancamentos.Dtos;
 /// <summary>
 /// Dados necessários para criar um lançamento.
 /// </summary>
-public sealed record CriarLancamentoRequest(
-    [property: Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
-    decimal Valor,
-    [property: EnumDataType(typeof(TipoLancamento))]
-    TipoLancamento Tipo,
-    [property: Required]
-    DateTime DataLancamento,
-    [property: StringLength(200)]
-    string? Descricao);
+public sealed class CriarLancamentoRequest
+{
+    [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+    public decimal Valor { get; init; }
+
+    [EnumDataType(typeof(TipoLancamento))]
+    public TipoLancamento Tipo { get; init; }
+
+    [Required]
+    public DateTime DataLancamento { get; init; }
+
+    [StringLength(200)]
+    public string? Descricao { get; init; }
+}
